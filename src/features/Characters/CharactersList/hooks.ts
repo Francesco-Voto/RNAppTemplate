f5d19b11390fc4d071289b0c7d582959c185423f
Francesco-Voto <francesco.voto@nearform.com>
2024-03-12 16:16:14 +0100

    feat: add benchmark

--- src/features/Characters/CharactersList/hooks.ts
+++ src/features/Characters/CharactersList/hooks.ts
@@ -1,7 +1,10 @@
 import {getCharactersKey, getCharacters, type Character} from '@services/api';
 import useSWRInfinite from 'swr/infinite';
+import Reactotron from 'reactotron-react-native';
 
 export function useGetCharactersList() {
+  const bench = Reactotron.benchmark('Normalize Characters');
+
   const {data, size, setSize} = useSWRInfinite(
     getCharactersKey,
     getCharacters,
@@ -10,14 +13,14 @@ export function useGetCharactersList() {
       revalidateFirstPage: false,
     },
   );
-
+  bench.step('Retrieving characters');
   const characters = data?.reduce(
     (acc: Character[], current: {results: Character[]}) => {
       return [...acc, ...current.results];
     },
     [],
   );
-
+  bench.stop('Normalize characters');
   return {
     data: characters,
     size,
